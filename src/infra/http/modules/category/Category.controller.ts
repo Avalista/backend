b0fee50792e137dfd12ef2d3fcd47b969cb0eea3
RamonSVieira <ramonvieira122333@gmail.com>
2025-05-20 17:30:04 -0300

    feat: add endpoint for editing category. (#12)

--- src/infra/http/modules/category/Category.controller.ts
+++ src/infra/http/modules/category/Category.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put } from '@nestjs/common';
 import { CreateCategoryUseCase } from 'src/modules/category/useCases/createCategoryUseCase/CreateCategoryUseCase';
 import { CreateCategoryBody } from './dtos/CreateCategoryBody';
 import { CategoryViewModel } from './viewModel/CategoryViewModel';
+import { EditCategoryUseCase } from 'src/modules/category/useCases/editCategoryUseCase/EditCategoryUseCase';
+import { EditCategoryBody } from './dtos/EditCategoryBody';
 
 @Controller('categories/')
 export class CategoryController {
-  constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
+  constructor(
+    private createCategoryUseCase: CreateCategoryUseCase,
+    private editCategoryUseCase: EditCategoryUseCase,
+  ) {}
+
+  //REPASSAR AQUI VALINDO QUAIS USUARIOS PODEM REQUISITAR ESSE ENDPOINTS
 
   @Post()
   async createCategory(@Body() body: CreateCategoryBody) {
@@ -14,5 +21,21 @@ export class CategoryController {
     const category = await this.createCategoryUseCase.execute({ name, color });
 
     return CategoryViewModel.toHttp(category);
+  }
+
+  @Put(':id')
+  async editCategory(
+    @Param('id') categoryId: string,
+    @Body() body: EditCategoryBody,
+  ) {
+    const { name, color } = body;
+    const mockUserId = '0';
+
+    await this.editCategoryUseCase.execute({
+      categoryId,
+      name,
+      color,
+      userId: mockUserId,
+    });
   }
 }
