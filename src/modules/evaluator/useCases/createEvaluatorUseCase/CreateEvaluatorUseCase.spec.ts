e9f3f3cd1a94fa3bc27d3118484e7488d0d3091a
RamonSVieira <ramonvieira122333@gmail.com>
2025-06-02 01:23:15 -0300

    feat: add useCase create evaluator. (#21)

---
+++ src/modules/evaluator/useCases/createEvaluatorUseCase/CreateEvaluatorUseCase.spec.ts
@@ -0,0 +1,44 @@
+import { compare } from 'bcrypt';
+import { EvaluatorRepositoryInMemory } from '../../repositories/EvaluatorRepositoryInMemory';
+import { CreateEvaluatorUseCase } from './CreateEvaluatorUseCase';
+
+let evaluatorRepositoryInMemory: EvaluatorRepositoryInMemory;
+let createEvaluatorUseCase: CreateEvaluatorUseCase;
+
+describe('Create Evaluator', () => {
+  beforeEach(() => {
+    evaluatorRepositoryInMemory = new EvaluatorRepositoryInMemory();
+    createEvaluatorUseCase = new CreateEvaluatorUseCase(
+      evaluatorRepositoryInMemory,
+    );
+  });
+
+  it('Should be able to create evaluator', async () => {
+    expect(evaluatorRepositoryInMemory.evaluators).toEqual([]);
+
+    const evaluator = await createEvaluatorUseCase.execute({
+      email: 'email@email.com',
+      name: 'name',
+      password: 'password',
+    });
+
+    expect(evaluatorRepositoryInMemory.evaluators).toEqual([evaluator]);
+  });
+
+  it('Should be able to create evaluator with password encrypted', async () => {
+    const evaluatorPasswordWithoutEncryption = 'password';
+
+    const evaluator = await createEvaluatorUseCase.execute({
+      email: 'email@email.com',
+      name: 'name',
+      password: evaluatorPasswordWithoutEncryption,
+    });
+
+    const userHasPassowrdEncrypted = await compare(
+      evaluatorPasswordWithoutEncryption,
+      evaluator.password,
+    );
+
+    expect(userHasPassowrdEncrypted).toBeTruthy();
+  });
+});
