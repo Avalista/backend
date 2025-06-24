61643f69035d0e8130cb2365b612a8f23863c7b4
RamonSVieira <ramonvieira122333@gmail.com>
2025-06-24 03:57:58 -0300

    fix: correct typo in variable name for SignInUseCase instance. (#68)

--- src/modules/auth/useCases/signInUseCase/SignInUseCase.spec.ts
+++ src/modules/auth/useCases/signInUseCase/SignInUseCase.spec.ts
@@ -3,19 +3,19 @@ import { SignInUseCase } from './SignInUseCase';
 import { makeEvaluator } from 'src/modules/evaluator/factories/EvaluatorFactory';
 import { EvaluatorPayload } from '../../models/EvaluatorPayload';
 
-let sigInUseCase: SignInUseCase;
+let signInUseCase: SignInUseCase;
 let jwtService: JwtService;
 
 describe('Sig In', () => {
   beforeEach(() => {
     jwtService = new JwtService({ secret: 'secret' });
-    sigInUseCase = new SignInUseCase(jwtService);
+    signInUseCase = new SignInUseCase(jwtService);
   });
 
   it('Should be able to create valid access_token', () => {
     const evaluator = makeEvaluator({});
 
-    const token = sigInUseCase.execute({ evaluator });
+    const token = signInUseCase.execute({ evaluator });
 
     const payload: EvaluatorPayload = jwtService.decode(token);
 
