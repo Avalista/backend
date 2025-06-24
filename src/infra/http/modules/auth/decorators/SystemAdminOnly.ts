9770029488b3d43683d6c72f7ff14187409194fb
RamonSVieira <ramonvieira122333@gmail.com>
2025-06-24 03:56:44 -0300

    fix: correct import path for IsSystemAdminGuard in SystemAdminOnly decorator. (#68)

--- src/infra/http/modules/auth/decorators/SystemAdminOnly.ts
+++ src/infra/http/modules/auth/decorators/SystemAdminOnly.ts
@@ -1,6 +1,6 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../guards/JwtAuth.guard';
-import { IsSystemAdminGuard } from '../guards/IsSytemAdmin.guard';
+import { IsSystemAdminGuard } from '../guards/IsSystemAdmin.guard';
 
 export function SystemAdminOnly() {
   return applyDecorators(UseGuards(JwtAuthGuard, IsSystemAdminGuard));
